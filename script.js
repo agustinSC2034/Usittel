23d31bc47ff7c4255ca6d111db7bc8e9fce61275
agustinSC2034 <aguustinn18@gmail.com>
2023-10-06 17:07:45 -0300

    actualizacionFinal-6-10

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+function verificarCobertura() {
+  const inputContainer = document.getElementById('inputContainer');
+  const titulo = document.getElementById('titulo');
+  const parrafo = document.getElementById('textoAbajoTitulo');
+  const botonVerificar = document.getElementById('botonVerificar');
+
+  const numeroIngresado = document.getElementById('inputNumero').value;
+
+  if (numeroIngresado === '111111' || numeroIngresado === '222222' || numeroIngresado === '333333') {
+    titulo.innerText = '¡Felicitaciones! Estás en la zona de cobertura.',
+    parrafo.innerText = 'Puedes dejarnos tus datos en el formulario que se encuentra al final de la pagina o escribirnos al mail: contacto@usittel.com.ar y un operador se contactará contigo. Tambien puedes llamaranos al 0800-199-4545';
+  } else {
+    titulo.innerText = 'No estás en la zona de cobertura.',
+    parrafo.innerText = 'Seguimos ampliandonos, pronto llegaremos a tu hogar';
+  }
+
+  // Oculta el contenedor del input y muestra el botón de volver
+  inputContainer.style.display = 'none';
+  botonVerificar.innerHTML = '<button class="button button-large button-rounded text-capitalize ls0" style="border-radius: 23px" onclick="volverAtras()">Volver</button>';
+}
+
+function volverAtras() {
+  const inputContainer = document.getElementById('inputContainer');
+  const titulo = document.getElementById('titulo');
+  const parrafo = document.getElementById('textoAbajoTitulo');
+  const botonVerificar = document.getElementById('botonVerificar');
+
+  // Muestra nuevamente el contenedor del input y restaura el contenido original
+  inputContainer.style.display = 'block';
+  titulo.innerText = '¿Estás en zona de usittel?';
+  botonVerificar.innerHTML = '<button class="button button-large button-rounded text-capitalize ls0" style="border-radius: 23px" onclick="verificarCobertura()">Verificar</button>';
+  document.getElementById('inputNumero').value = ''; // Borra el contenido del input
+  parrafo.innerText = '';
+}
