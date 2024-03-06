119e9365ffde293fcb1cdbe30390891db0d10228
agustinSC2034 <aguustinn18@gmail.com>
2024-03-06 02:26:46 -0300

    captchaV3

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -7,14 +7,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $client = htmlspecialchars($_POST["client"]);
   $message = htmlspecialchars($_POST["message"]);
 
+  // Verificar si hay campos vacíos
+  if (empty($name) || empty($email) || empty($subject) || empty($client) || empty($message)) {
+    echo '<script>alert("Por favor, completa todos los campos del formulario.");</script>';
+    echo '<script>setTimeout(function() { window.location.href = "index.html"; }, 1000);</script>';
+    exit;
+  }
+  
   // CAPTCHA GOOGLE
   $ip = $_SERVER['REMOTE_ADDR'];
   $captcha = $_POST['g-recaptcha-response'];
   $secretkey = "6LeX74spAAAAAApSGnvOXALpuyaLvLkcrqtkY6_7";
 
-  $respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
+  // Validar el captcha utilizando cURL
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+    'secret' => $secretkey,
+    'response' => $captcha,
+    'remoteip' => $ip
+  )));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+  curl_close($ch);
 
-  $atributos = json_decode($respuesta, TRUE);
+  $atributos = json_decode($response, TRUE);
 
   if (!$atributos['success']) {
     echo '<script>alert("El captcha no fue validado.");</script>';
@@ -22,13 +40,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   }
   // FIN CODIGO CAPTCHA
-
-  // Verificar si hay campos vacíos
-  if (empty($name) || empty($email) || empty($subject) || empty($client) || empty($message)) {
-    echo '<script>alert("Por favor, completa todos los campos del formulario.");</script>';
-    echo '<script>setTimeout(function() { window.location.href = "index.html"; }, 1000);</script>';
-    exit;
-  }
 
   // Validar el correo electrónico
   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -52,8 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 
   // Procesar los datos (por ejemplo, enviar un correo electrónico)
-  //$to = "formulariousittel@gmail.com";
-  $to = "aguustinn18@gmail.com";
+  $to = "formulariousittel@gmail.com";
   $subject = "Nuevo mensaje de contacto";
   $body = "Nombre: " . $name . "\n";
   $body .= "Correo electrónico: " . $email . "\n";
@@ -68,5 +78,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
   }
 }
-
 ?>
